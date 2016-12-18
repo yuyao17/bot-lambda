4715ce26b80ceacdcd70472c4dae7c8cac7a32d6
物井達也 <t.monoi117@gmail.com>
2016-12-18 23:29:17 +0900

    auth

--- index.js
+++ index.js
@@ -3,34 +3,24 @@ exports.run = (req, context, callback) => {
   console.log('start lambda');
   console.log('req:' + JSON.stringify(req, undefined, 1));
 
-  errMsg = _validate(req, context);
-  if(errMsg){
-    console.error(errMsg);
-    // console.error('req:' + JSON.stringify(req, undefined, 1));
-    // console.error('context:' + JSON.stringify(context, undefined, 1));
-    return callback(new Error(errMsg));
-  }
-
   _setup();
+
+  try {
+    _validate(req, context);
+    _authenticate(req);
+  } catch (err) {
+    console.error(err);
+    return callback(err);
+  }
 
   const handler = require('handler');
   handler(req, context, (err) => {
     if(err){
-      // console.error('req:' + JSON.stringify(req, undefined, 1));
-      // console.error('context:' + JSON.stringify(context, undefined, 1));
       return callback(err);
     }
 
     callback(null);
   });
-}
-
-_validate = (req, context) => {
-  errMsg = null;
-  if(!req || !req.bot_name || !req.event_name || !req.channel_name){
-    errMsg = 'invalid request';
-  }
-  return errMsg;
 }
 
 _setup = () => {
@@ -39,4 +29,21 @@ _setup = () => {
   global.async = require('async');
   global._ = require('lodash');
   global.CONFIG = require('conf/config')
+}
+
+_validate = (req, context) => {
+  if(!req || !req.bot_name || !req.event_name || !req.channel_name){
+    throw new Error('invalid request');
+  }
+}
+
+_authenticate = (req) => {
+  // api経由の場合は認証を行う
+  if (req.from_api) {
+    if (req.bot_name === 'lunch_bot') {
+      if (req.token !== CONFIG.LUNCH_BOT.OUTGOING_TOKEN) {
+        throw new Error('authentication reject');
+      }
+    }
+  }
 }
--- src/conf/config.js
+++ src/conf/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   LUNCH_BOT: {
     BOT_ID: process.env.LUNCH_BOT_ID,
-    TOKEN: process.env.LUNCH_BOT_TOKEN
+    TOKEN: process.env.LUNCH_BOT_TOKEN,
+    OUTGOING_TOKEN: process.env.LUNCH_BOT_OUTGOING_TOKEN
   }
 };
