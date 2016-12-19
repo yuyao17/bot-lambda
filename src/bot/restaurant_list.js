9f7a96c537c8457df2150c90a1af765a614e9dd8
akoba <akoba0708@gmail.com>
2016-12-19 14:48:38 +0900

    Update restaurant_list.js

--- src/bot/restaurant_list.js
+++ src/bot/restaurant_list.js
@@ -33,5 +33,45 @@ module.exports = [
     name: "うどん",
     tabelog: "https://tabelog.com/tokyo/A1316/A131603/13004531/",
     emoji: ":curry:"
+  },
+  {
+    name: "ジンギスカン霧島",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13195060/",
+    emoji: ":sheep:"
+  },
+  {
+    name: "路地裏",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13126019/",
+    emoji: ":kichi:"
+  },
+  {
+    name: "平太周",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13002046/",
+    emoji: ":kichi:"
+  },
+  {
+    name: "陳家私菜",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13050281/",
+    emoji: ":fire:"
+  },
+  {
+    name: "ホットスプーン",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13024328/",
+    emoji: ":curry:"
+  },
+  {
+    name: "フランクリンアベニュー",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13001556/",
+    emoji: ":hamburger:"
+  },
+  {
+    name: "広州市場",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13001555/",
+    emoji: ":ramen:"
+  },
+  {
+    name: "ナキューズ",
+    tabelog: "https://tabelog.com/tokyo/A1316/A131603/13185399/",
+    emoji: ":tomato:"
   }
 ];
